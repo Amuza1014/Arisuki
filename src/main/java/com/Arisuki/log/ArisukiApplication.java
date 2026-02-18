7e20bbd614880bbf397852f4621ebdc9ed7c70ea
3030680 <3030680@30306-80>
2026-02-18 13:07:07 +0900

    0218/1307

--- src/main/java/com/Arisuki/log/ArisukiApplication.java
+++ src/main/java/com/Arisuki/log/ArisukiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArisukiApplication {
 
 	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(ArisukiApplication.class, args);
 	}
