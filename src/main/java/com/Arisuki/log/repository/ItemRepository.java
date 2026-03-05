87f7dbf022b7314cbe802c8d34b66eb06e7f57b4
YuyaMatsubayashi0401 <y_matsubayashi0401@outlook.jp>
2026-03-05 15:15:34 +0900

    検索機能

--- src/main/java/com/Arisuki/log/repository/ItemRepository.java
+++ src/main/java/com/Arisuki/log/repository/ItemRepository.java
@@ -3,6 +3,8 @@ package com.Arisuki.log.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.Arisuki.log.entity.InformationEntity;
@@ -16,4 +18,11 @@ public interface ItemRepository extends JpaRepository<InformationEntity, Integer
 
     // タイムライン用：全投稿をIDの降順（新しい順）で取得
     List<InformationEntity> findAllByOrderByIdDesc();
+    
+    @Query("SELECT i FROM InformationEntity i WHERE " +
+            "(:category IS NULL OR :category = '' OR i.category = :category) AND " +
+            "(:keyword IS NULL OR :keyword = '' OR i.title LIKE %:keyword% OR i.creator LIKE %:keyword%) " +
+            "ORDER BY i.id DESC")
+     List<InformationEntity> searchItems(@Param("category") String category, @Param("keyword") String keyword);
+ 
 }
